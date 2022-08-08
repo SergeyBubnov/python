1b7c8440694fb57127392bd400df8f7b60d7fb96
Practice <i@welc.me>
2022-08-08 23:15:26 +0300

    это предпоследняя версия

--- pcp.py
+++ pcp.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 class Perceptron(object):
     """Perceptron classifier.
 
@@ -64,5 +63,3 @@ class Perceptron(object):
     def predict(self, X):
         """Return class label after unit step"""
         return np.where(self.net_input(X) >= 0.0, 1, -1)
-
-        from matplotlib.colors import ListedColormap
